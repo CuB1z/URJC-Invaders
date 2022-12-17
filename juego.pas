4a3ed88e4d7f7126f62236d91a4e52a5b7ed3d87
Porti <a@a.com>
2022-12-17 20:18:22 +0100

    WORKING - Static bullets

--- juego.pas
+++ juego.pas
@@ -13,13 +13,19 @@ var
     gameThreadFlag:integer; // Almacena un valor respectivo a la ejecucion del juego (0 = salir, otro = continuar)
     board:t_board; // Matrix de elementos del juego
     obj_player:t_player; // Creamos un registro para el jugador
+    obj_bulletsData:t_bulletsData; // Array de datos de las balas
 begin
     // Initial setup
-    gameThreadFlag := 1;
-    obj_player.i := 0;
+    gameThreadFlag := 1; // Set game flag to 1 (running)
+    // Setup for player parameters
+    obj_player.i := 0; 
     obj_player.j := 0;
+    obj_player.health := 100;
+    // Setup for bullet parameters
+    obj_bulletsData.n := 0;
+    // Reset the game to default values and print it
     resetBoard(board);
-    printBoard(board);
+    printBoard(board, obj_bulletsData, obj_player);
 
     while gameThreadFlag <> 0 do begin
         
@@ -28,17 +34,22 @@ begin
 
         // Que hacer cuando presionamos una tecla
         if input <> 0 then begin
-            gameThreadFlag := parseKey(input, obj_player.i, obj_player.j);
-    
+            case input of
+                11779: gameThreadFlag := 0; // Exit with Ctrl+C
+                65313: if (obj_player.i > 0) then  obj_player.i:=obj_player.i-1; // Flecha arriba
+                65315: if (obj_player.j > 0) then  obj_player.j:=obj_player.j-1; // Flecha izqda
+                65319: if (obj_player.i < HEIGHT-PLAYER_H) then  obj_player.i:=obj_player.i+1; // Flecha abajo
+                65317: if (obj_player.j < WIDTH-PLAYER_W) then  obj_player.j:=obj_player.j+1; // Flecha decha
+                14624: playerShoot(obj_bulletsData, obj_player); // Espacio
+            end;
         end;
-        
         
         // Refresh game frame
         clrscr();
         resetBoard(board);
-        writeln(' [i = ', obj_player.i, '] [j = ', obj_player.j,']'); // Header info
-        setPlayerPos(board, obj_player.i, obj_player.j);  
-        printBoard(board);
+        // Write frame
+        printBoard(board, obj_bulletsData, obj_player);
+        
 
         // Game speed
         delay(GAME_SPEED);
--- space_invaders_module.pas
+++ space_invaders_module.pas
@@ -4,31 +4,45 @@
 unit space_invaders_module;
 
 
-interface // ----------------------------------------------------
+interface // ============================================================[ INTERFACE ]>
 // Modulos
 uses crt, dos, keyboard;
+
 // Parametros del programa
 const 
     WIDTH = 50;    // Board size
     HEIGHT = 10;
     PLAYER_W = 11; // Size of the player design
     PLAYER_H = 3;
+
 // Tipos
 type 
-    t_board = array[0..HEIGHT, 0..WIDTH] of char;
     t_player = RECORD
-        i,j:integer;
+        i,j, health:integer;
     end;
+    t_bullet = RECORD
+        i,j, damage:integer;
+    end;
+    t_bulletsData = RECORD
+        n:integer; // Number of bullets
+        bulletsList:array[0..40] of t_bullet;
+    end;
+    t_board = array[0..HEIGHT, 0..WIDTH] of char;
+
 // Subprogramas
 function listenKeys():uint16;
-procedure printBoard(board:t_board);
+procedure printBoard(board:t_board; obj_bulletsData:t_bulletsData; obj_player:t_player);
 procedure resetBoard(var board:t_board); 
-function parseKey(input:uint16; var i,j:integer):integer;
 procedure strPush(var board:t_board; i,j:integer; str:string);
-procedure setPlayerPos(var board:t_board; i,j:integer);
+procedure writePlayerPos(var board:t_board; i,j:integer);
+procedure playerShoot(var obj_bulletsData:t_bulletsData; obj_player:t_player);
+procedure writeBullets(var board:t_board; obj_bulletsData:t_bulletsData);
 
-implementation // ----------------------------------------------------
 
+
+implementation // ============================================================[ IMPLEMENTATION ]>
+
+// ----------------------------------------------------
 // Leer inputs
 function listenKeys():uint16;
 var kbdEvent:TKeyEvent; kbdCode: word;
@@ -47,20 +61,30 @@ begin
 end;
 
 
+// ----------------------------------------------------
 // Imprimir el juego
-procedure printBoard(board:t_board); // IDEA: Imprimir linea por linea en vez de caracter por caracter
+procedure printBoard(board:t_board; obj_bulletsData:t_bulletsData; obj_player:t_player); // IDEA: Imprimir linea por linea en vez de caracter por caracter
 var i,j:integer; buff:string;
 begin
 
+    writeBullets(board, obj_bulletsData); // Write bullets possition to the board
+    writePlayerPos(board, obj_player.i, obj_player.j); // Write player position to the board
+
+    writeln(' [i = ', obj_player.i, '] [j = ', obj_player.j,'] []'); // Header info
+
+    // Print game matrix
     for i:=0 to HEIGHT do begin
         buff := '';
         for j:=0 to WIDTH do
             buff:=buff+board[i,j];
         writeln(buff);
     end;
+
+
 end;
 
 
+// ----------------------------------------------------
 // Borrar datos de la matriz del juego
 procedure resetBoard(var board:t_board); 
 var i,j:integer;
@@ -70,22 +94,8 @@ begin
             board[i,j] := ' ';
 end;
 
-// Evaluar accion en base a la tecla resionada
-function parseKey(input:uint16; var i,j:integer):integer; begin
-    parseKey := 1; // Default flag 1: Keep running
-    
-    case input of
 
-        11779: parseKey := 0; // Exit with Ctrl+C
-        65313: if (i > 0) then  i:=i-1; // Flecha arriba
-        65315: if (j > 0) then  j:=j-1; // Flecha izqda
-        65319: if (i < HEIGHT-PLAYER_H) then  i:=i+1; // Flecha abajo
-        65317: if (j < WIDTH-PLAYER_W) then  j:=j+1; // Flecha decha
-
-    end;
-end;
-
-
+// ----------------------------------------------------
 procedure strPush(var board:t_board; i,j:integer; str:string);
 var x,n:integer;
 begin
@@ -96,7 +106,8 @@ begin
 end;
 
 
-procedure setPlayerPos(var board:t_board; i,j:integer); begin
+// ----------------------------------------------------
+procedure writePlayerPos(var board:t_board; i,j:integer); begin
     // Si se cambia el diseño, tambiend deben cambiarse PLAYER_H, PLAYER_W
     strPush(board, i,j,   '\   />');
     strPush(board, i+1,j, '=|==[#]===>');
@@ -104,5 +115,25 @@ procedure setPlayerPos(var board:t_board; i,j:integer); begin
 end;
 
 
-// --------------------------------------------------------------------------
+// ----------------------------------------------------
+procedure writeBullets(var board:t_board; obj_bulletsData:t_bulletsData); 
+var x:integer;
+begin
+    for x:=0 to obj_bulletsData.n-1 do begin
+        board[obj_bulletsData.bulletsList[x].i, obj_bulletsData.bulletsList[x].j] := '-';
+    end;
+end;
+
+
+// ----------------------------------------------------
+procedure playerShoot(var obj_bulletsData:t_bulletsData; obj_player:t_player); begin
+    // Spawn 1 bullet relative to the player position
+    obj_bulletsData.bulletsList[obj_bulletsData.n].i := obj_player.i+1;
+    obj_bulletsData.bulletsList[obj_bulletsData.n].j := obj_player.j+PLAYER_W;
+
+    obj_bulletsData.n := obj_bulletsData.n + 1;
+end;
+
+
+// =========================================================================================
 end.
