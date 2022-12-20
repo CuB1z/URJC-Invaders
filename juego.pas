627395633ac907c496b214e9097b800ae38f670f
Porti <a@a.com>
2022-12-20 09:11:48 +0100

    Merge PC

--- juego.pas
+++ juego.pas
@@ -3,29 +3,33 @@ uses crt, space_invaders_module, keyboard;
 
 
 // --------------------------------------------[ PARAMETROS DEL PROGRAMA ]>
-const GAME_SPEED = 10; // En (milis): mas bajo = mas rapido
 
 
 // --------------------------------------------------[ FUNCION MAIN LOOP ]>
 function mainLoop():integer; 
 var
+    clock:uint16;
     input:uint16; // Almacena los carateres presionados
     gameThreadFlag:integer; // Almacena un valor respectivo a la ejecucion del juego (0 = salir, otro = continuar)
     board:t_board; // Matrix de elementos del juego
     obj_player:t_player; // Creamos un registro para el jugador
     obj_bulletsData:t_bulletsData; // Array de datos de las balas
+    obj_enemiesData:t_enemiesData;
 begin
     // Initial setup
     gameThreadFlag := 1; // Set game flag to 1 (running)
+    clock := 0;
     // Setup for player parameters
     obj_player.i := 0; 
     obj_player.j := 0;
     obj_player.health := 100;
-    // Setup for bullet parameters
+    // Setup for game parameters
     obj_bulletsData.n := 0;
+    obj_enemiesData.n := 0;
     // Reset the game to default values and print it
     resetBoard(board);
     resetBullets(obj_bulletsData);
+    resetEnemies(obj_enemiesData);
     printBoard(board);
 
     while gameThreadFlag <> 0 do begin
@@ -38,10 +42,10 @@ begin
         if input <> 0 then begin
             case input of
                 11779: gameThreadFlag := 0; // Exit with Ctrl+C
-                65313: if (obj_player.i > 0) then  obj_player.i:=obj_player.i-1; // Flecha arriba
-                65315: if (obj_player.j > 0) then  obj_player.j:=obj_player.j-1; // Flecha izqda
-                65319: if (obj_player.i < HEIGHT-PLAYER_H) then  obj_player.i:=obj_player.i+1; // Flecha abajo
-                65317: if (obj_player.j < WIDTH-PLAYER_W) then  obj_player.j:=obj_player.j+1; // Flecha decha
+                65313: if (obj_player.i > 0) then  obj_player.i := obj_player.i-1; // Flecha arriba
+                65315: if (obj_player.j > 0) then  obj_player.j := obj_player.j-1; // Flecha izqda
+                65319: if (obj_player.i < HEIGHT-PLAYER_H+1) then  obj_player.i := obj_player.i+1; // Flecha abajo
+                65317: if (obj_player.j < BORDER-PLAYER_W) then  obj_player.j := obj_player.j+1; // Flecha decha
                 14624: playerShoot(obj_bulletsData, obj_player); // Espacio
             end;
         end;
@@ -50,16 +54,25 @@ begin
         clrscr();
         resetBoard(board);
         // Update board values
-        updateBoard(board, obj_bulletsData, obj_player);
+        updateBoard(board, obj_bulletsData, obj_player, obj_enemiesData);
         // Write frame
-        writeln(' [i = ', obj_player.i, '] [j = ', obj_player.j,'] []'); // Header info
+        writeln(' [i = ', obj_player.i, '] [j = ', obj_player.j,'] [Health = ', obj_player.health ,'] [Score = ', obj_player.score ,'] [CLOCK: ', clock:6, ']'); // Header info
         printBoard(board);
+
+        // Handle enemy events
+        enemyEvents(clock, obj_enemiesData);
         
         // Update the possition of the dynamic objects of the game
-        updateGameDynamics(obj_bulletsData);
+        updateGameDynamics(clock, obj_bulletsData, obj_enemiesData);
+
+        // Check hits
+        checkHits(board, obj_bulletsData, obj_player, obj_enemiesData);
         
         // Game speed
         delay(GAME_SPEED);
+
+        // Game clock
+        clock := clock+1 mod CLOCK_RESET;
     end;
 end;
 
@@ -69,6 +82,7 @@ begin
     // Program Config 
     InitKeyboard();
     cursoroff();
+    randomize();
 
     // Game main loop
     mainLoop();
--- space_invaders_module.pas
+++ space_invaders_module.pas
@@ -1,5 +1,6 @@
 
-
+// Colision de bala con jugador por coordenada j 
+// Colision de bala con nave por coordenadas
 
 unit space_invaders_module;
 
@@ -9,39 +10,60 @@ interface // ============================================================[ INTER
 uses crt, dos, keyboard;
 
 // Parametros del programa
-const 
-    WIDTH = 50;    // Board size
+const
+    WIDTH = 80;    // Board size
     HEIGHT = 10;
-    PLAYER_W = 11; // Size of the player design
+    PLAYER_W = 9; // Size of the player design
     PLAYER_H = 3;
-    MAX_BULLETS = 20; // Maximum number of bullets
+    BORDER = 50;
+    MAX_BULLETS = 100; // Maximum number of player bullets
+    GAME_SPEED = 10; // En (milis): mas bajo = mas rapido
+    CLOCK_RESET = 10000; // The max value the clock will reach before return to 0
+    MAX_ENEMIES = 5;
 
 // Tipos
-type 
-    t_player = RECORD
-        i,j, health:integer;
+type
+    t_player = RECORD // Player object
+        i,j, health, score:integer;
     end;
-    t_bullet = RECORD
-        i,j, damage:integer;
-        active:boolean;
+    t_bullet = RECORD // Bullet object
+        // Direction takes (1=forward) or (-1=backwards)
+        // Owner takes (1=player), (2=enemy)
+        i,j, direction, owner:int8; 
+        active:boolean; // Display or not
+        design:char; // Graphics of the object
     end;
-    t_bulletsData = RECORD
+    t_bulletsData = RECORD // Bullets list object
         n:integer; // Number of bullets
         bulletsList:array[0..MAX_BULLETS] of t_bullet;
+    end;
+    t_enemy = RECORD // Enemy object
+        i,j, health:integer;
+        active:boolean;
+        design:string[10];
+    end;
+    t_enemiesData = RECORD // Bullets list object
+        n:integer; // Number of bullets
+        enemiesList:array[0..MAX_ENEMIES] of t_enemy;
     end;
     t_board = array[0..HEIGHT, 0..WIDTH] of char;
 
 // Subprogramas
 function listenKeys():uint16;
 procedure printBoard(board:t_board);
-procedure updateBoard(var board:t_board; obj_bulletsData:t_bulletsData; obj_player:t_player);
-procedure resetBoard(var board:t_board); 
+procedure updateBoard(var board:t_board; obj_bulletsData:t_bulletsData; obj_player:t_player; obj_enemiesData:t_enemiesData);
+procedure resetBoard(var board:t_board);
 procedure strPush(var board:t_board; i,j:integer; str:string);
 procedure writePlayerPos(var board:t_board; i,j:integer);
-procedure playerShoot(var obj_bulletsData:t_bulletsData; obj_player:t_player);
 procedure writeBullets(var board:t_board; obj_bulletsData:t_bulletsData);
-procedure updateGameDynamics(var obj_bulletsData:t_bulletsData);
+procedure writeEnemies(var board:t_board; obj_enemiesData:t_enemiesData);
+procedure playerShoot(var obj_bulletsData:t_bulletsData; obj_player:t_player);
+procedure enemyShoot(var obj_bulletsData:t_bulletsData; obj_enemy:t_enemy);
+procedure updateGameDynamics(clock:uint16; var obj_bulletsData:t_bulletsData; var obj_enemiesData:t_enemiesData);
 procedure resetBullets(var obj_bulletsData:t_bulletsData);
+procedure enemyEvents(clock:uint16; var obj_enemiesData:t_enemiesData);
+procedure resetEnemies(var obj_enemiesData:t_enemiesData);
+procedure checkHits(board:t_board; var obj_bulletsData:t_bulletsData; var obj_player:t_player; var obj_enemiesData:t_enemiesData); 
 
 implementation // ============================================================[ IMPLEMENTATION ]>
 
@@ -66,41 +88,49 @@ end;
 
 // ----------------------------------------------------
 // Imprimir el juego
-procedure printBoard(board:t_board); 
+procedure printBoard(board:t_board);
 var i,j:integer; buff:string;
 begin
 
     // Print game matrix
+    write('+');
+    for i:=0 to WIDTH do write('-');
+    writeln('+');
     for i:=0 to HEIGHT do begin
         buff := '';
         for j:=0 to WIDTH do
             buff:=buff+board[i,j];
-        writeln(buff);
+        writeln('|'+buff+'|');
     end;
+    write('+');
+    for i:=0 to WIDTH do write('-');
+    writeln('+');
+
 
 end;
 
 
 // ----------------------------------------------------
-procedure updateBoard(var board:t_board; obj_bulletsData:t_bulletsData; obj_player:t_player); begin
+procedure updateBoard(var board:t_board; obj_bulletsData:t_bulletsData; obj_player:t_player; obj_enemiesData:t_enemiesData); begin
     writeBullets(board, obj_bulletsData); // Write bullets possition to the board
     writePlayerPos(board, obj_player.i, obj_player.j); // Write player position to the board
+    writeEnemies(board, obj_enemiesData); // Write player position to the board
 end;
 
 
 // ----------------------------------------------------
 // Borrar datos de la matriz del juego
-procedure resetBoard(var board:t_board); 
+procedure resetBoard(var board:t_board);
 var i,j:integer;
 begin
-    for i:=0 to HEIGHT do 
+    for i:=0 to HEIGHT do
         for j:=0 to WIDTH do
             board[i,j] := ' ';
 end;
 
 
 // ----------------------------------------------------
-procedure resetBullets(var obj_bulletsData:t_bulletsData); 
+procedure resetBullets(var obj_bulletsData:t_bulletsData);
 var x:integer;
 begin
     for x:=0 to MAX_BULLETS do begin
@@ -115,7 +145,7 @@ var x,n:integer;
 begin
     n := length(str);
     for x:=0 to n-1 do begin
-        board[i,j+x] := str[x+1];
+        if j+x <= WIDTH then board[i,j+x] := str[x+1];
     end;
 end;
 
@@ -124,20 +154,29 @@ end;
 procedure writePlayerPos(var board:t_board; i,j:integer); begin
     // Si se cambia el diseño, tambiend deben cambiarse PLAYER_H, PLAYER_W
     strPush(board, i,j,   '\   />');
-    strPush(board, i+1,j, '=|==[#]===>');
+    strPush(board, i+1,j, '=|=[#]==>');
     strPush(board, i+2,j, '/   \>');
 end;
 
 
 // ----------------------------------------------------
-procedure writeBullets(var board:t_board; obj_bulletsData:t_bulletsData); 
+procedure writeBullets(var board:t_board; obj_bulletsData:t_bulletsData);
 var x:integer;
 begin
-    for x:=0 to MAX_BULLETS do 
+    for x:=0 to MAX_BULLETS do
         if obj_bulletsData.bulletsList[x].active then begin
-            // board[1, x] := '-';
-            board[obj_bulletsData.bulletsList[x].i, obj_bulletsData.bulletsList[x].j] := '-';
-            
+            board[obj_bulletsData.bulletsList[x].i, obj_bulletsData.bulletsList[x].j] := obj_bulletsData.bulletsList[x].design;
+        end;
+end;
+
+
+// ----------------------------------------------------
+procedure writeEnemies(var board:t_board; obj_enemiesData:t_enemiesData);
+var x:integer;
+begin
+    for x:=0 to MAX_ENEMIES do
+        if obj_enemiesData.enemiesList[x].active then begin
+            strPush(board, obj_enemiesData.enemiesList[x].i, obj_enemiesData.enemiesList[x].j, obj_enemiesData.enemiesList[x].design);
         end;
 end;
 
@@ -150,24 +189,113 @@ procedure playerShoot(var obj_bulletsData:t_bulletsData; obj_player:t_player); b
     obj_bulletsData.bulletsList[obj_bulletsData.n].i := obj_player.i+1;
     obj_bulletsData.bulletsList[obj_bulletsData.n].j := obj_player.j+PLAYER_W;
     obj_bulletsData.bulletsList[obj_bulletsData.n].active := true;
+    obj_bulletsData.bulletsList[obj_bulletsData.n].design := '-';
+    obj_bulletsData.bulletsList[obj_bulletsData.n].direction := 1;
+    obj_bulletsData.bulletsList[obj_bulletsData.n].owner := 1;
 
     obj_bulletsData.n := obj_bulletsData.n + 1;
 end;
 
+// ----------------------------------------------------
+procedure enemyShoot(var obj_bulletsData:t_bulletsData; obj_enemy:t_enemy); begin
+    if obj_bulletsData.n >= MAX_BULLETS then obj_bulletsData.n := 0;
+
+    // Spawn 1 bullet relative to the player position
+    obj_bulletsData.bulletsList[obj_bulletsData.n].i := obj_enemy.i;
+    obj_bulletsData.bulletsList[obj_bulletsData.n].j := obj_enemy.j-1;
+    obj_bulletsData.bulletsList[obj_bulletsData.n].active := true;
+    obj_bulletsData.bulletsList[obj_bulletsData.n].design := 'o';
+    obj_bulletsData.bulletsList[obj_bulletsData.n].direction := -1;
+    obj_bulletsData.bulletsList[obj_bulletsData.n].owner := 2;
+
+    obj_bulletsData.n := obj_bulletsData.n + 1;
+end;
 
 // ----------------------------------------------------
-procedure updateGameDynamics(var obj_bulletsData:t_bulletsData); 
+procedure updateGameDynamics(clock:uint16; var obj_bulletsData:t_bulletsData; var obj_enemiesData:t_enemiesData);
 var x:integer;
+    shootRate:boolean;
 begin
 
+    // --- BULLETS ---
     // Forward bullets possition
     for x:=0 to MAX_BULLETS do begin
-        if obj_bulletsData.bulletsList[x].j >= WIDTH then
-            obj_bulletsData.bulletsList[x].active := false; 
-        obj_bulletsData.bulletsList[x].j := obj_bulletsData.bulletsList[x].j + 1;
+        if (obj_bulletsData.bulletsList[x].active = true) then begin // Move only active bullets
+
+            // Disappear bullet when hit the edge
+            if (obj_bulletsData.bulletsList[x].j <= 0) or (obj_bulletsData.bulletsList[x].j >= WIDTH) then
+                obj_bulletsData.bulletsList[x].active := false;
+
+            // Move bullet position
+            obj_bulletsData.bulletsList[x].j := obj_bulletsData.bulletsList[x].j + obj_bulletsData.bulletsList[x].direction;
+        end;
+    end;
+
+    // --- ENEMIES ---
+    shootRate := (clock mod 10) = 0;
+    for x:=0 to MAX_ENEMIES do begin
+        if (obj_enemiesData.enemiesList[x].active = true) then begin 
+            if ( shootRate and (random(5)=0) ) then enemyShoot(obj_bulletsData, obj_enemiesData.enemiesList[x]);
+        end;
     end;
 end;
 
+
+// ----------------------------------------------------
+procedure enemyEvents(clock:uint16; var obj_enemiesData:t_enemiesData); begin
+    if obj_enemiesData.n >= MAX_ENEMIES then obj_enemiesData.n := 0;
+
+
+    if (clock mod 50 = 0) then begin // Spawn enemy
+        obj_enemiesData.enemiesList[obj_enemiesData.n].active := true;
+        obj_enemiesData.enemiesList[obj_enemiesData.n].i := random(HEIGHT-2)+1;
+        obj_enemiesData.enemiesList[obj_enemiesData.n].j := BORDER + random(WIDTH-BORDER-5);
+        obj_enemiesData.enemiesList[obj_enemiesData.n].design := '=(o)';
+
+        obj_enemiesData.n := obj_enemiesData.n+1;
+    end;
+end;
+
+
+// ----------------------------------------------------
+procedure resetEnemies(var obj_enemiesData:t_enemiesData);
+var x:integer;
+begin
+    for x:=0 to MAX_ENEMIES do begin
+        obj_enemiesData.enemiesList[x].active := false;
+    end;
+end;
+
+
+// ----------------------------------------------------
+procedure checkHits(board:t_board; var obj_bulletsData:t_bulletsData; var obj_player:t_player; var obj_enemiesData:t_enemiesData); 
+var x,k,pi,pj,bi,bj:integer;
+begin
+
+    pi := obj_player.i; // Shorthand for player possition
+    pj := obj_player.j;
+
+    for x:=0 to MAX_BULLETS do
+        if obj_bulletsData.bulletsList[x].active then begin // Active bullets
+
+            bi := obj_bulletsData.bulletsList[x].i; // Shorthand for bullet possition
+            bj := obj_bulletsData.bulletsList[x].j;
+
+            if (board[bi, bj] <> ' ') then begin // Bullet hit enemy/player
+                // Player hitted
+                if (pi <= bi) and (pi+PLAYER_H >= bi) and (pj < bj) and (pj+PLAYER_W >= bj) then obj_player.health := obj_player.health-10
+                // Enemy hitted
+                else if (bj > BORDER) then begin
+                    obj_player.score := obj_player.score+100;
+                    for k:=0 to MAX_ENEMIES do
+                        if obj_enemiesData.enemiesList[k].active then if (obj_enemiesData.enemiesList[k].i = bi) and (obj_enemiesData.enemiesList[k].j >= bj-1) then
+                            obj_enemiesData.enemiesList[k].active := false;
+                end;
+
+                obj_bulletsData.bulletsList[x].active := false;
+            end;
+        end;
+end;
 
 
 // =========================================================================================
