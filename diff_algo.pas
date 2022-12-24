a437f1cb22f59f1a00b3032a008d1da5c295d765
Porti <a@a.com>
2022-12-24 12:57:12 +0100

    Diff algorith finish, not integrated

--- diff_algo.pas
+++ diff_algo.pas
@@ -7,28 +7,37 @@ const
     WIDTH = 20;
 type 
     t_board = array[0..HEIGHT, 0..WIDTH] of char;
-    t_boardInts = array[0..HEIGHT, 0..WIDTH] of integer;
+    t_boardInt = array[0..HEIGHT, 0..WIDTH] of integer;
 var 
     board1, board2:t_board;
+    board3:t_boardInt;
     i,j:integer;
 
 procedure printBoard(b:t_board); var i,j:integer; begin
     for i:=0 to HEIGHT do begin
         for j:=0 to WIDTH do 
-            write(b[i,j] + ' ');
+            write(b[i,j], ' ');
+        writeln;
+    end;
+end;
+procedure printBoard(b:t_boardInt); var i,j:integer; begin
+    for i:=0 to HEIGHT do begin
+        for j:=0 to WIDTH do 
+            write(b[i,j], ' ');
         writeln;
     end;
 end;
 
 // ==============================================================================
-// Return a board of integers (changes) where (0=no_change) (N=Sucesive_chars_changed)
+// Return a board of integers (changes) where (0=no_change) (1=Changed)
 // Diffs are from new related to old
-procedure diffBoard(old, new:t_board; var changes:t_boardInts);
-var i,j,tmp:integer;
+procedure diffBoard(old, new:t_board; var changes:t_boardInt);
+var i,j:integer;
 begin
     for i:=0 to HEIGHT do
         for j:=0 to WIDTH do begin
-            if new[i,j] <> old[i,j] then changes[i,j]
+            if new[i,j] <> old[i,j] then changes[i,j] := 1
+            else changes[i,j] := 0;
         end;
 end;
 // ==============================================================================
@@ -48,15 +57,16 @@ begin
         for j:=0 to WIDTH do 
             board2[i,j] := ' ';
     board2[2,4] := 'x';
-    board2[6,1] := 'x';
-    board2[9,7] := 'x';
+    board2[1,5] := 'x';
+    board2[2,6] := 'x';
     // -------------
     writeln('-------------------------------------------------');
     printBoard(board1);
     printBoard(board2);
     writeln('-------------------------------------------------');
-    diffBoard(board1, board2)
-    writeln('-------------------------------------------------');
+
+    diffBoard(board1, board2, board3);
+    printBoard(board3);
     // -------------
     // diffBoard();
 
